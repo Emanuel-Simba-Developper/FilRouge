a536da7e92f222b52570f4a325d6ac52d45bd5c6
Emanuel Simba <emanuel.simba@yahoo.com>
2024-07-29 19:38:10 +0200

    Ajout sytème routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,6 @@
+import Route from "./Route.js";
+
+//defenir les routes
+export const allRoutes = [new Route("/", "Accueil", "pages/home.html")];
+//le titre s'affiche comme ceci : Route.title - websiteName
+export const websiteName = "Quai Antique";
